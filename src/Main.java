95852b1bf922d906c6de25203ae9c718be07019f
vitorGabr <macstudios.info@gmail.com>
2023-10-23 13:50:20 -0300

    removido espaçamento

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         new Controller(new Janela(), new DaoConcreto()).init();
-        // Contro daoBooks = new DaoBooks();
-        // try {
-        // if (daoBooks.addBook("Teste de livro", 201, "teste5", (float) 30, 435, 1)) {
-        // System.out.println("Livro adicionado com sucesso!");
-        // } else {
-        // System.out.println("Livro não adicionado!");
-        // }
-        // } catch (SQLIntegrityConstraintViolationException e) {
-        // e.printStackTrace();
-        // }
     }
 
 }
